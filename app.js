06af2c928e612525b079a48b12e7db317a6282f6
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-01-04 22:40:41 +0000

    read and writing files

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var fs = require('fs')
 
-fs.readFile('readMe.txt' , 'utf8', function(error,data){
-    fs.writeFile('writeMe.txt' , data );
+fs.unlink("./writeMe.txt" , function(){
+    fs.rmdir('stuff');
+    
 });
-// fs.writeFileSync('writeMe.txt', readMe);
 
-// var myEmitter = new events.EventEmitter();
 
-// myEmitter.on(`someEvent`, function(mssg){
-//     console.log(mssg)
-// });
 
-// myEmitter.emit('someEvent' , 'the event was emitted'); 
+fs.mkdir('stuff' , function(){
+    fs.readFile('readMe.txt', 'utf-8' , function(error,data){
+        fs.writeFile('./writeMe.txt', data)
+    })
+}) 
