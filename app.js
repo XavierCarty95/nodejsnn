2e1a41dfcabd79cb19f49ca9dc88dd4209055f19
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-01-09 21:38:17 +0000

    writeable and readable streams

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var fs = require('fs')
+var http = ("http");
+var fs = require('fs');
 
-fs.unlink("./writeMe.txt" , function(){
-    fs.rmdir('stuff');
-    
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+myReadStream.on('data' , function(chunk){
+    console.log('new chunk received'); 
+    console.log(chunk);
+    myWriteStream.write(chunk);
 });
 
-
-
-fs.mkdir('stuff' , function(){
-    fs.readFile('readMe.txt', 'utf-8' , function(error,data){
-        fs.writeFile('./writeMe.txt', data)
-    })
-}) 
